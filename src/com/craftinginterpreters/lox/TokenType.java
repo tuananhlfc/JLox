2a59646ee525664072b003fd78c62f0cdc89aff2
tuananhlfc <tuananhlfc@gmail.com>
2020-10-26 22:50:02 +0700

    Create Token and TokenType

---
+++ src/com/craftinginterpreters/lox/TokenType.java
@@ -0,0 +1,15 @@
+package com.craftinginterpreters.lox;
+
+public enum TokenType {
+    // Single-character tokens
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // One or two chracter tokens
+
+    // Literals
+
+    // Keywords
+
+    EOF
+}
